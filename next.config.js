aed9831d9bf68580761255c6ee965bde1125ebbc
eslavadev <danieleslava52@gmail.com>
2023-07-31 18:14:01 -0500

    Fix problem nexts img and hooks

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['flowbite.s3.amazonaws.com', "v1.tailwindcss.com"]
-    },
-    experimental: {
-        serverActions: true,
-    },
-    env: {
-        API_URL: process.env.API_URL
-    },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
